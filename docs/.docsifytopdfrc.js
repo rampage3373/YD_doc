1d9c489b0cf9bdf5df07c2d8fd113e6649f1a9ff
rampage3373 <hujz@hanlinit.com>
2021-12-01 16:21:10 +0800

    update

--- docs/.docsifytopdfrc.js
+++ docs/.docsifytopdfrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    contents: [ "docs/_sidebar.md" ], // array of "table of contents" files path
+    contents: [ "_sidebar.md", ], // array of "table of contents" files path
     pathToPublic: "pdf/readme.pdf", // path where pdf will stored
     pdfOptions: "<options for puppeteer.pdf()>", // reference: https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagepdfoptions
     removeTemp: true, // remove generated .md and .html or not
--- docs/_navbar.md
+++ docs/_navbar.md
@@ -3,3 +3,6 @@
 * [语言/Language]
     * [简体中文](/)
     * [English](/en-us)
+* [版本/Version]
+    * [V1.0](/v1)
+    * [V1.1](/v2)
